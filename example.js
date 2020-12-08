af1e4e24a5e4bd5d7dc9481ea44867a7cd49c260
jy <jaey5oung@naver.com>
2020-12-08 23:24:16 +0900

    data type

--- example.js
+++ example.js
@@ -28,55 +28,137 @@ if (sum / 5 === 0) {
 //상황에 따라 변수는 값을 재설정할수 있다
 
 const sum = 5 + 10
-let result = false;
+let result = false
 
 if (sum / 3 === 0) {
-    console.log("야호1")
-    result = true
-  }
-  if (sum / 5 === 0) {
-    console.log("야호2")
-    result = true
-  }
-  console.log(result);
-  
-  //블럭
-  {
-    const name = 'jang';
-    console.log(name);
-    
-  }
+  console.log("야호1")
+  result = true
+}
+if (sum / 5 === 0) {
+  console.log("야호2")
+  result = true
+}
+console.log(result)
 
-  // console.log(name);
+//블럭
+{
+  const name = "jang"
+  console.log(name)
+}
 
-  //밖에서 안으로
+// console.log(name);
 
-  let age = 37;
-  {
-    age++;
-  }
+//밖에서 안으로
 
-  //중첩
-  //블럭은 블럭안에서 변수를선언할때 그 블럭범위에서만 사용할수있고 블럭밖에서 먼저 선언하게되면 어디서든 블럭범위에서 사용할수있다 하지만 블럭안에서 선언된것은 밖에서 사용은 불가능하다
+let age = 37
+{
+  age++
+}
 
-  //var es5까지만 사용되었지만
-  
-  var c = 0;
-  {
-    c++;
-    console.log(c);
-    
-  }
-  
-  {
-    var d = 0;
-    console.log(d);
-    
-  }
+//중첩
+//블럭은 블럭안에서 변수를선언할때 그 블럭범위에서만 사용할수있고 블럭밖에서 먼저 선언하게되면 어디서든 블럭범위에서 사용할수있다 하지만 블럭안에서 선언된것은 밖에서 사용은 불가능하다
 
-  console.log(d);
+//var es5까지만 사용되었지만
 
-  //원래 const나 let을 사용할땐 블럭안에선언되어잇는걸 밖에서 사용하지못하지만 var같은경우는 블럭이 있든없든 함수처리를 하기때문에 블럭안에서 선언이되도 밖에서 사용할수있다
+var c = 0
+{
+  c++
+  console.log(c)
+}
+
+{
+  var d = 0
+  console.log(d)
+}
+
+console.log(d)
+
+//원래 const나 let을 사용할땐 블럭안에선언되어잇는걸 밖에서 사용하지못하지만 var같은경우는 블럭이 있든없든 함수처리를 하기때문에 블럭안에서 선언이되도 밖에서 사용할수있다
 
 // var키워드와 hoisting
-  
+
+//호이스팅은 es5언어 명세 및 그 이전 표준 명세에서 사용된적이 없는 용어이다
+//아래있는 선언을(만) 끌어올리는 현상을 호이스팅 var 키워드에는 적용되지만 let이나 const는 적용이안된다
+
+//함수먼저
+
+function hello1() {
+  console.log("hello1")
+}
+hello1()
+
+//함수의 호출을 먼저
+hello2()
+
+function hello2() {
+  console.log("hello2")
+}
+
+age = 6
+age++
+console.log(age)
+
+var age
+
+//Data Types 자료형
+//변수가 가지는 고정 타입이 없다
+//하지만 타입이 없는것은 아니다
+
+let whatever = "Jang"
+
+whatever = 37
+
+whatever = true
+
+//기본타입 Boolean , Null , Undefined , Number , String , Symbol
+
+//Boolean
+const isTrue = true
+const isFalse = false
+
+console.log(isTrue, typeof isTrue)
+console.log(isFalse, typeof isFalse)
+
+const a = new Boolean(false)
+
+console.log(a, typeof a)
+
+if (a) {
+  console.log("false?")
+}
+
+const b = Boolean(false)
+
+console.log(b, typeof b)
+
+//Null 값을 가지고있지만 비어잇는 값을가지고있는것
+
+const a = null
+console.log(a, typeof a)
+
+//객체타입
+
+//Undefined
+
+let b
+
+console.log(b, typeof b)
+
+b = undefined
+
+console.log(b, typeof b)
+
+if (a == b) {
+  console.log(a == b)
+}
+
+if (a === b) {
+  console.log(a === b)
+}
+// Null과 undefined 는 ==일땐 서로같지만 ===일땐 타입까지 서로 다르게나온다
+
+//Number
+
+const a = 37
+
+console.log(a, typeof a)
