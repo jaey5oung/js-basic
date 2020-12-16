008aad652d3b1344051887c0e78f18571f0be349
jy <jaey5oung@naver.com>
2020-12-16 23:16:29 +0900

    생성자 함수

--- example.js
+++ example.js
@@ -674,5 +674,53 @@ const hello4 = (name) => {
 
 const hello5 = (name) => `hello5 ${name}`;
 
+//생성자 함수를 이용하여 새로운 객체를 만들어 내는 방법
+
+function Person(name, age) {
+  console.log(this);
+
+  this.name = name;
+  this.age = age;
+}
+
+const p = new Person('Mark', 37);
+console.log(p, p.name, p.age);
+
+const q = new Person('Jang', 15);
+console.log(q, q.name, q.age);
+
+// const cat =(name,age)=>{
+//   this.name = name;
+//   this.age = age
+// }
+// arrow function 에서는 this를 사용할수 없다
+
+//함수를 호출하면 함수를 만들어서 리턴
+
+function plus(base) {
+  return function (num) {
+    return base + num;
+  };
+}
+
+const plus5 = plus(5);
+console.log(plus(10));
+
+const plus7 = plus(7);
+console.log(plus7(8));
+
+//함수를 인자로 하여 함수를 호출
+
+function hello(c) {
+  console.log('hello');
+  c();
+}
+hello(function () {
+  console.log('콜백');
+});
 
 
+
+// 객체(object) 함수,클래스(틀)=> 객체,개체,object
+
+//function 틀(){}=> new 틀() 생성자함수로 객체만들기
