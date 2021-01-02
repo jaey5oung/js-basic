b79f27de19a76d49efea63240d99a5cdd9f00190
jy <jaey5oung@naver.com>
2021-01-02 22:44:50 +0900

    Function 점수를성적등급으로변환하기

--- example2.js
+++ example2.js
@@ -203,8 +203,35 @@ function hello(name) {
 hello('velopert');
 
 
-//점수를 성적등급으로 변환하기
+
 //새해 다시준비하기 및 복습
 
 
 // --------------------------------------------------------------------------------------------------------------
+
+//점수를 성적등급으로 변환하기
+
+function getGrade(score) {
+  if (score === 100) {
+    return 'A+';
+  } else if (score >= 90) {
+    return 'A';
+  } else if (score === 89) {
+    return 'B+';
+  } else if (score >= 80) {
+    return 'B';
+  } else if (score === 79) {
+    return 'C+';
+  } else if (score >= 70) {
+    return 'C';
+  } else if (score === 69) {
+    return 'D+';
+  } else if (score >= 60) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
+
+const grade = getGrade(90);
+console.log(grade);
