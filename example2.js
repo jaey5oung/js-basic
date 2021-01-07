55d3a53a223566977ebdb4f811cb3f6130aef60f
jy <jaey5oung@naver.com>
2021-01-07 20:00:12 +0900

    반복문

--- example2.js
+++ example2.js
@@ -429,6 +429,92 @@ console.log(objects.length);
 
 //반복문
 
-for (let i = 0; i < 10; i++){
-  
+//for문
+for (let i = 0; i < 10; i++) {
+  console.log(i);
 }
+
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+const names = ['멍멍이', '야옹이', '멍뭉이'];
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+//while문
+let i = 0;
+let isFun = false;
+
+//while문에 조건은 트루가된다 예를들어 isFun ===false 도 !isFun으로 고쳐쓸수있다
+while (isFun === false) {
+  i++;
+  if (i === 30) {
+    isFun = true;
+  }
+}
+
+//while문은 항상 조건이 마지막엔 펄스가 되야된다
+
+//for of
+
+const numbers = [10, 20, 30, 40, 50];
+
+for (let number of numbers) {
+  console.log(number);
+}
+
+//for in
+
+const doggy = {
+  name: '멍멍이',
+  sound: '멍멍이',
+  age: 2,
+};
+
+for (let key in doggy) {
+  console.log(`${key}: ${doggy[key]}`);
+}
+
+console.log(Object.entries(doggy));
+console.log(Object.keys(doggy));
+console.log(Object.vales(doggy));
+//엔트리는 벨류와 키값을 둘다 알아보는것이구 키는 왼쪽 키값만 벨류는 오른쪽 벨류값을 알아보는 함수이다
+
+//break and continue
+
+for (let i = 0; i < 10; i++) {
+  if (i === 2) continue;
+  console.log(i);
+  if (i === 5) break;
+}
+//컨티뉴는 특정 조건이 만족됬을때 그조건은 스킵이되고 조건이 반복되고 (다음)
+//브레이크가 걸렸을때 그 시점에서 조건을 끝마치게된다 (끝)
+
+// --------------------------------------------------------------------------------------------------------------
+
+function sumOf(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+
+const result = sumOf([1, 2, 3, 4, 5]);
+console.log(result);
+
+
+function biggerThanThree(numbers) {
+  const array = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 3) {
+      array.push(numbers[i]);
+    }
+  }
+  return array;
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+console.log(biggerThanThree(numbers)); // [4, 5, 6, 7]
